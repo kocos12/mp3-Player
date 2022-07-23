92e0caed8218c787636fe2c288e71c5ed664bed7
wojcikmichal <wojcikmichal@student.agh.edu.pl>
2022-07-23 21:56:25 +0200

    initial commit

---
+++ src/main/java/pl/javastart/mp3player/controller/ControlPaneController.java
@@ -0,0 +1,53 @@
+package pl.javastart.mp3player.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
+
+public class ControlPaneController {
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private ToggleButton playButton;
+
+    @FXML
+    private Button previousButton;
+
+    @FXML
+    private Slider progressSlider;
+
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    private TextField messageTextField;
+
+    public Button getNextButton() {
+        return nextButton;
+    }
+
+    public ToggleButton getPlayButton() {
+        return playButton;
+    }
+
+    public Button getPreviousButton() {
+        return previousButton;
+    }
+
+    public Slider getProgressSlider() {
+        return progressSlider;
+    }
+
+    public Slider getVolumeSlider() {
+        return volumeSlider;
+    }
+
+    public TextField getMessageTextField() {
+        return messageTextField;
+    }
+}
